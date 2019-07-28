122bec6517395431b3f76836c2fa7513872209ad
shadaxv <aleksandern1116@gmail.com>
2019-07-28 15:48:06 +0200

    Updates

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-264c7a633237d5e45288.js"
+    "url": "webpack-runtime-48b6d410533ded75325b.js"
   },
   {
     "url": "app-830851e8f86b92320b3b.js"
@@ -36,7 +36,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "d24985ba7aca0f4210de4c813f2a0035"
+    "revision": "5728debef9831c426f4ce2d8f586174c"
   },
   {
     "url": "styles.983267e4f816f7202fc7.css"
@@ -60,15 +60,15 @@ self.__precacheManifest = [
     "url": "1-38e42093dec2847d4d45.js"
   },
   {
-    "url": "component---src-pages-404-js-663c1fffaa6a800ee6b9.js"
+    "url": "component---src-pages-404-js-c9410bac22a8f8e337fb.js"
   },
   {
     "url": "page-data/404.html/page-data.json",
-    "revision": "37eecf0894c424abfc7818cd79de7919"
+    "revision": "6a5b6339f64f79a6eb3d9e336c395340"
   },
   {
     "url": "page-data/offline-plugin-app-shell-fallback/page-data.json",
-    "revision": "2df5edb3cebdafb868ec3c4f4bb73b3b"
+    "revision": "3dec6f5d2a6d0a1b402ea40ba790a6c5"
   },
   {
     "url": "manifest.webmanifest",
